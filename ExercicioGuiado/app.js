308e25e29be1908ed53480e05f9ac6a06376b6f9
ENZO PIETRO BOEIRA <enzo_boeira@estudante.sesisenai.org.br>
2026-06-03 21:23:41 -0300

    Blabla update

--- ExercicioGuiado/app.js
+++ ExercicioGuiado/app.js
@@ -51,3 +51,86 @@ btnAumentarJs.addEventListener('click', () => {
     barraJs.style.width = novoNivel + '%';
 
 });
+
+const btnEditarNome        = document.querySelector('#btn-editar-nome');
+const formEdicao           = document.querySelector('#form-edicao');
+const inputNome            = document.querySelector('#input-nome');
+const inputProfissao       = document.querySelector('#input-profissao');
+const nomeUsuario          = document.querySelector('#nome-usuario');
+const profissaoUsuario     = document.querySelector('#profissao-usuario');
+
+btnEditarNome.addEventListener('click', () => {
+
+    formEdicao.style.display = 'block';
+
+    inputNome.value = nomeUsuario.textContent;
+    inputProfissao.value = profissaoUsuario.textContent;
+
+});
+
+const btnSalvar = document.querySelector('#btn-salvar');
+const btnCancelar = document.querySelector('#btn-cancelar');
+
+btnSalvar.addEventListener('click', () => {
+
+    const novoNome         = inputNome.value.trim();
+    const novaProfissao    = inputProfissao.value.trim();
+
+    if(novoNome === ''){
+        alert('O seu safadinho! O nome não pode ser vazio, igual sua barriga imensa depois de vir da escola.');
+        return;
+    }
+
+    nomeUsuario.textContent = novoNome;
+    profissaoUsuario.textContent = novaProfissao || 'Sem profissão';
+
+    formEdicao.style.display = 'none';
+
+});
+
+btnCancelar.addEventListener('click', () => {
+
+    formEdicao.style.display = 'none';
+
+});
+
+/* Trocar Foto de Perfil*/
+
+const btnTrocarFoto         = document.querySelector('#btn-trocar-foto');
+const fotoOpcoes            = document.querySelector('#foto-opcoes');
+const fotoPerfil            = document.querySelector('#foto-perfil')
+
+btnTrocarFoto.addEventListener('click', () => {
+
+    if(fotoOpcoes.style.display === 'block'){
+        fotoOpcoes.style.display = 'none';
+    }else{
+        fotoOpcoes.style.display = 'block';
+        formEdicao.style.display = 'none';
+    }
+
+});
+
+const todasFotos = document.querySelectorAll('.foto-opcao');
+
+
+
+todasFotos.forEach(opcao => {
+
+    opcao.addEventListener('click', () => {
+
+    // <img dat-seed = "Felix"> -> opcao.dataset.seed === "Felix"
+    const seed = opcao.dataset.seed;
+
+    fotoPerfil.src = `https://api.dicebear.com/7.x/adventurer/svg?seed=${seed}`;
+
+    todasFotos.forEach(f = f.classList.remove('selecionada'));
+    opcao.classList.add('selecionada');
+
+    fotoOpcoes.style.display = 'none';
+
+    console.log('Avatar trocado para:', seed)
+
+    });
+
+});
